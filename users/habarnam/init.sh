21a92a06e9c5216f12b3b8111d8e6c360dd488eb
Marius Orcsik <marius@federated.id>
2021-01-04 11:11:12 +0100

    Update public keys

--- users/habarnam/init.sh
+++ users/habarnam/init.sh
@@ -6,9 +6,10 @@ test -z "${_usergroups}" && _usergroups="sudo"
 test -z "${_usershell}" && _usershell="/bin/bash"
 test -z "${_userpackages}" && _userpackages="libpam-systemd neovim powerline python-powerline mc bash-completion awscli htop tmux"
 test -z "${_userkeys}" && _userkeys=(
- "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBATB9Su5K2N5EOKSeYHdecIDqYnq0kM3QyCOsSziMn8rDHR1pls5WDTvVl4/fg7W9OsWexg1HVKHXfTXmTOh7hE="
- "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAEsd9sZcNWOlzTIX9VTSFbY3V3aGtkEtfPU3XO3C2lDFL5ORQFws85eQ4mtw5TxfBEmYBItSXCBBEKBYgywbCL2AQH1NueikJUV6K8lyQ4qxKU8wW0H5PDXHbELXo2pCDzgADJbN2CKaPaBVpq/2klVW736euwJvy4xWZY9cjsDeRzlcw==")
+ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjsLsDQ49ASkbqn0MbsWGF0+D+YPC1FvHIZLejzoFcN"
+ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmtU4Qj6o6SFPtlm/pBtqjYRadL/2jJgP3UOyp/5WoH"
+)
 
 _userhome="/home/${_username}"
-########### 
+###########
 
