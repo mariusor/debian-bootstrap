74ff3414a338f2f79575885f96d02de06addc8d7
Marius Orcsik <marius@federated.id>
2021-01-04 13:22:58 +0100

    Remove some packages

--- users/habarnam/init.sh
+++ users/habarnam/init.sh
@@ -4,7 +4,7 @@ test -z "${_username}" && _username=$(basename "$(dirname "$BASH_SOURCE")")
 test -z "${_userid}" && _userid=1199
 test -z "${_usergroups}" && _usergroups="sudo"
 test -z "${_usershell}" && _usershell="/bin/bash"
-test -z "${_userpackages}" && _userpackages="libpam-systemd neovim powerline python-powerline mc bash-completion awscli htop tmux"
+test -z "${_userpackages}" && _userpackages="neovim mc bash-completion htop tmux"
 test -z "${_userkeys}" && _userkeys=(
  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjsLsDQ49ASkbqn0MbsWGF0+D+YPC1FvHIZLejzoFcN"
  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmtU4Qj6o6SFPtlm/pBtqjYRadL/2jJgP3UOyp/5WoH"
